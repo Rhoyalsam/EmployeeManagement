e919ef4cb0f50cf9c4f6461f019c9ac36a2e04ce
Samuel Ogunremi <samuel.ogunremi@wemabank.com>
2023-05-22 14:36:40 +0100

    Add project files.

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello From MVC";
+        }
+
+    }
+}
